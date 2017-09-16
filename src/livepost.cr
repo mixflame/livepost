78e80af0e779dac279c35c5ccef0505645e3e3a0
cerulean <mixflame@mixfla.me>
2017-09-16 16:08:57 -0600

    production test

--- src/livepost.cr
+++ src/livepost.cr
@@ -11,12 +11,11 @@ require "./models/**"
 require "./views/**"
 require "../config/*"
 
-
 amber = Amber::Server
 
-if amber.settings.env == "production"
-  puts "production mode. daemonizing."
-  Daemonize.daemonize(stdout: "/home/mixflame/livepost/production.log", stderr: "/home/mixflame/livepost/error.log", stdin: "/dev/null")
-end
+# if amber.settings.env == "production"
+#   puts "production mode. daemonizing."
+#   Daemonize.daemonize(stdout: "/home/mixflame/livepost/production.log", stderr: "/home/mixflame/livepost/error.log", stdin: "/dev/null")
+# end
 
 amber.instance.run
