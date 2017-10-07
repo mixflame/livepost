39a2cb9839f0f06a1e13e68ea47c00b5ac40130b
Jonathan Silverman <jsilverman2@gmail.com>
2017-10-07 16:52:52 -0600

    apply serverside and css message size limits

--- src/controllers/chat_controller.cr
+++ src/controllers/chat_controller.cr
@@ -9,11 +9,12 @@ class ChatController < ApplicationController
   end
 
   def send_message
-    UserSocket.broadcast("message", "chat_room:chat", "message:new", {:author => HTML.escape(session[:handle].to_s), :message => HTML.escape(params["message"])})
+    message = HTML.escape(params["message"])[0..512]
+    UserSocket.broadcast("message", "chat_room:chat", "message:new", {:author => HTML.escape(session[:handle].to_s), :message => message})
     client = Mongo::Client.new "mongodb://localhost:27017/livepost"
     db = client["live_post"]
     chat_messages = db["chat_messages"]
-    chat_messages.insert({"message" => HTML.escape(params["message"]), "author" => HTML.escape(session[:handle].to_s)})
+    chat_messages.insert({"message" => message, "author" => HTML.escape(session[:handle].to_s)})
     response.content_type = "text/json"
     {error: "Success", csrf: csrf_tag}.to_json
   end
